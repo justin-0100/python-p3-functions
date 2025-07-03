41a763fb9755feb858e2b90567adcfcf8d392e8b
Justin <justinagui988@gmail.com>
2025-07-03 09:42:08 +0300

    implement required functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
+# 1. No arguments, just prints
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+# 2. One argument, prints personalized greeting
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
+# 3. One argument with default value
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
+# 4. Adds two numbers and returns result
 def add(num1, num2):
-    pass
+    return num1 + num2
 
+# 5. Returns half of the number
 def halve(number):
-    pass
+    return number / 2
